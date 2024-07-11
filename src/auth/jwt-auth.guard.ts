943762c5857f8ad95ff8dd12b7a183cdf896f9e3
Tay Nguyen <pttnguyen528@gmail.com>
2024-07-11 11:06:29 +0700

    push code subscribers

--- src/auth/jwt-auth.guard.ts
+++ src/auth/jwt-auth.guard.ts
@@ -41,15 +41,20 @@ export class JwtAuthGuard extends AuthGuard('jwt') {
 
     //check permission
     const targetMethod = request.method;
-    const targetEndpoint = request.route?.path;
+    const targetEndpoint = request.route?.path as string;
 
     const permissions = user?.permissions ?? [];
 
-    const isExist = permissions.find(
+    let isExist = permissions.find(
       (permission) =>
         targetMethod === permission.method &&
         targetEndpoint === permission.apiPath,
     );
+
+    if (targetEndpoint.startsWith('/api/v1/auth')) {
+      isExist = true;
+    }
+
     if (!isExist) {
       throw new ForbiddenException(
         'Bạn không có quyền để truy cập endpoint này',
--- src/subscribers/subscribers.module.ts
+++ src/subscribers/subscribers.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { SubscribersService } from './subscribers.service';
 import { SubscribersController } from './subscribers.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Subscriber, SubscriberSchema } from './schemas/subscriber.schemas';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: Subscriber.name, schema: SubscriberSchema },
+    ]),
+  ],
   controllers: [SubscribersController],
-  providers: [SubscribersService]
+  providers: [SubscribersService],
 })
 export class SubscribersModule {}
